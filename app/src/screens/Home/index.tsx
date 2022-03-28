2058638279eb368c497c2bc036e6d5a2667a6da4
Marlon <marlon@pop-os.localdomain>
2022-03-28 15:32:04 -0300

    iniciando

---
+++ app/src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import {Hello} from './style'
+import {View, Text} from 'react-native'
+
+export default function Home(){
+    return(
+        <View>
+            <Hello>
+                Hello World!
+            </Hello>
+        </View>
+    )
+
+}
---
+++ app/src/screens/Home/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const Hello = styled.Text`
+    background-color: blue;
+    color: white;
+    height: 10vh;
+    width: 80vw;
+    border-radius: 15px;
+`
